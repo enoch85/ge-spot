5cc1368e6513af42a521f44dc7173e765f057f0b
Daniel Hansson <mailto@danielhansson.nu>
2025-04-05 23:57:36 +0200

    Update config_flow.py

--- custom_components/ge_spot/config_flow.py
+++ custom_components/ge_spot/config_flow.py
@@ -1,6 +1,2 @@
 """Config flow for GE-Spot integration."""
-from .config_flow.implementation import GSpotConfigFlow, GSpotOptionsFlow  # pylint: disable=unused-import
-
-# Home Assistant expects to find ConfigFlow class in this file
-# Re-export as ConfigFlow for Home Assistant to discover
-ConfigFlow = GSpotConfigFlow
+from .config_flow import GSpotConfigFlow, GSpotOptionsFlow  # pylint: disable=unused-import
