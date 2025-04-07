5c2e2b58e798366dbc7b8f4d3c3b8d908c8dbf93
Daniel Hansson <mailto@danielhansson.nu>
2025-04-07 19:42:42 +0200

    Create __init__.py

---
+++ custom_components/ge_spot/sensor/__init__.py
@@ -0,0 +1,22 @@
+"""Sensor functionality for electricity price data."""
+from .electricity import async_setup_entry
+from .base import BaseElectricityPriceSensor
+from .price import (
+    PriceValueSensor,
+    ExtremaPriceSensor,
+    TomorrowExtremaPriceSensor,
+    TomorrowAveragePriceSensor,
+    TimestampAttributeMixin,
+    TomorrowSensorMixin,
+)
+
+__all__ = [
+    "async_setup_entry",
+    "BaseElectricityPriceSensor",
+    "PriceValueSensor",
+    "ExtremaPriceSensor",
+    "TomorrowExtremaPriceSensor",
+    "TomorrowAveragePriceSensor",
+    "TimestampAttributeMixin",
+    "TomorrowSensorMixin",
+]
