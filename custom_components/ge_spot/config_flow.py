831b889ce5645efbb93b87f41e4239b6d44fc706
Daniel Hansson <mailto@danielhansson.nu>
2025-04-08 12:27:40 +0200

    Update config_flow.py

--- custom_components/ge_spot/config_flow.py
+++ custom_components/ge_spot/config_flow.py
@@ -1,2 +1,3 @@
 """Config flow for GE-Spot integration."""
-from .config_flow import GSpotConfigFlow, GSpotOptionsFlow  # pylint: disable=unused-import
+from .config_flow.implementation import GSpotConfigFlow  # pylint: disable=unused-import
+from .config_flow.options import GSpotOptionsFlow  # pylint: disable=unused-import
