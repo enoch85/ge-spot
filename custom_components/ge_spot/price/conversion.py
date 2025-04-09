db19a59c7617fbe916e7fe29f28af6765f3b1632
Daniel Hansson <mailto@danielhansson.nu>
2025-04-09 19:34:47 +0200

    Update conversion.py

--- custom_components/ge_spot/price/conversion.py
+++ custom_components/ge_spot/price/conversion.py
@@ -4,7 +4,7 @@ from typing import Optional, Dict, Any
 
 from .currency import convert_to_subunit, get_subunit_name
 from .energy import convert_energy_unit
-from ..utils.exchange_service import ExchangeRateService
+from ..utils.exchange_service import ExchangeRateService, get_exchange_service
 
 _LOGGER = logging.getLogger(__name__)
 
